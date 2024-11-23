1877258bd1cbf35e730e0c2704866800805c87d5
falex69 <falex@free.fr>
2024-11-23 19:54:36 +0000

    first commit

---
+++ gmt.py
@@ -0,0 +1,13 @@
+# GMT check and push to the forum
+
+import re
+import requests
+
+channel = "https://www.youtube.com/user/benoistrousseauandli"
+#channel = "https://www.youtube.com/"
+html = requests.get(channel + "/videos").text
+info = re.search('(?<={"label":").*?(?="})', html).group()
+url = "https://www.youtube.com/watch?v=" + re.search('(?<="videoId":").*?(?=")', html).group()
+
+print(info)
+print(url)
