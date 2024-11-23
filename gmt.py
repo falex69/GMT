01c8f9139bb5badfb23695fa481664deca9fd2af
falex69 <falex@free.fr>
2024-11-23 19:58:49 +0000

    direct url

--- gmt.py
+++ gmt.py
@@ -3,8 +3,8 @@
 import re
 import requests
 
-channel = "https://www.youtube.com/user/benoistrousseauandli"
-#channel = "https://www.youtube.com/"
+#channel = "https://www.youtube.com/user/benoistrousseauandli"
+channel = "https://www.youtube.com/@BenoistRousseau/streams"
 html = requests.get(channel + "/videos").text
 info = re.search('(?<={"label":").*?(?="})', html).group()
 url = "https://www.youtube.com/watch?v=" + re.search('(?<="videoId":").*?(?=")', html).group()
