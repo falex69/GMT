93e7c1bef3101c59d0855a7421eb6d14c3b91e6f
falex69 <falex@free.fr>
2024-11-23 20:05:52 +0000

    cleanup

--- gmt.py
+++ gmt.py
@@ -3,14 +3,10 @@
 import re
 import requests
 
-#channel = "https://www.youtube.com/user/benoistrousseauandli"
 channel = "https://www.youtube.com/@BenoistRousseau/streams"
 html = requests.get(channel + "/videos").text
 info = re.search('(?<={"label":").*?(?="})', html).group()
 url = "https://www.youtube.com/watch?v=" + re.search('(?<="videoId":").*?(?=")', html).group()
-
-#print(info)
-#print(url)
 
 print("1) Verification du dernier direct")
 print(f"   Video : {info}")
