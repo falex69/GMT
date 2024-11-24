ba6d5cbc40260d2d6d51edfa7aea3fbf22f95da0
falex69 <falex@free.fr>
2024-11-24 15:32:24 +0000

    Todo list

--- gmt.py
+++ gmt.py
@@ -22,6 +22,12 @@ if choix == "yes":
     andlil_login_url = "https://www.andlil.com/forum/ucp.php?mode=login&redirect=index.php"
     r = requests.get(andlil_login_url).text
     #print(r)
+    #TODO : Debug de la sessionde login pour ensuite poster une page.
+    # CErtaineemn,t en 3 temps :
+    # 1) Connexion a la page de login (récupération des sid et autres cookie)
+    # 2) POST du formulaire de login
+    # 3) Get de la page d'index
+    # 4) Post de la page cible.
 elif choix == "no":
     print("Ok !")
 else:
